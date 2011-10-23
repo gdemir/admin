471a813ba51666ebe6b8a5aad12b3ee3472c634c
gdemir <gdemir@bil.omu.edu.tr>
2011-10-23 12:10:41 +0300

    standart db

--- config/init.php
+++ config/init.php
@@ -5,4 +5,9 @@ F3::config(".f3.ini");
 F3::set('DB', new DB('mysql:host=localhost;port=3306;dbname=' . F3::get('dbname'), F3::get('dbuser'), F3::get('dbpass')));
 F3::set('SR', '/' . strtok($_SERVER["SCRIPT_NAME"], '/'));
 
+$sql = explode(';', file_get_contents('config/schema.db'));
+foreach ($sql as $raw)
+if ($query = trim($raw))
+	DB::sql($query);
+
 ?>
