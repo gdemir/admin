19ed93786fa396a267f9a9d899f3dcb773aa981d
gdemir <gdemir@bil.omu.edu.tr>
2011-10-25 11:41:00 +0300

    action:get filter:require_login

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ function page($template, $title, $layout='layout') {
 	F3::set('template', $template);
 	echo Template::serve($layout . '.htm');
 }
+function rl() { // require_login, before_filter
+	if (! F3::get('SESSION.admin'))  return F3::reroute('/');
+}
 function download() {
 	page('download', 'Tablo Indirme');
 }
@@ -42,25 +45,26 @@ function login() {
 	page('login', 'Yönetici Paneli');
 }
 
-F3::route("GET  /*",        'login');        // login page
-F3::route("POST /login",    'login.php');    // login action
-F3::route('GET  /logout',   'logout.php');   // logout action
-F3::route('POST /table',    'table.php');    // table action
+F3::route("GET  /*",        'login');          // login page
+F3::route("POST /login",    'login.php');      // login action
+F3::route('GET  /logout',   'logout.php');     // logout action
+F3::route('POST /table',    'table.php');      // table action
 
-F3::route("GET  /info",     'info');         // info page
-F3::route("GET  /review",   'review.php');   // review action
-F3::route("GET  /download", 'download');     // csv download page
-F3::route("POST /download", 'download.php'); // csv download action
-F3::route("GET  /upload",   'upload');       // csv upload page
-F3::route("POST /upload",   'upload.php');   // csv upload action
-F3::route("GET  /new",      'new.php');      // new page
-F3::route("POST /add",      'add.php');      // new action
-F3::route("GET  /find",     'find');         // find page
-F3::route("POST /find",     'find.php');     // find action
-F3::route("GET  /show",     'show');         // show page
-F3::route("POST /del",      'del.php');      // del action
-F3::route("POST /edit",     'edit.php');     // edit action
-F3::route("POST /update",   'update.php');   // update action
+F3::route("GET  /new",      'rl; new.php');    // new page
+F3::route("GET  /find",     'rl; find');       // find page
+F3::route("GET  /show",     'rl; show');       // show page
+F3::route("GET  /review",   'rl; review.php'); // review action
+F3::route("GET  /download", 'rl; download');   // csv download page
+F3::route("GET  /upload",   'rl; upload');     // csv upload page
+F3::route("GET  /info",     'rl; info');       // info page
+
+F3::route("POST /download", 'download.php');   // csv download action
+F3::route("POST /upload",   'upload.php');     // csv upload action
+F3::route("POST /add",      'add.php');        // new action
+F3::route("POST /find",     'find.php');       // find action
+F3::route("POST /del",      'del.php');        // del action
+F3::route("POST /edit",     'edit.php');       // edit action
+F3::route("POST /update",   'update.php');     // update action
 
 F3::run();
 
